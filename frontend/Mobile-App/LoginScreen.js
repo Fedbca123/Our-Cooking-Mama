653958d71d765188c61035aca63dcbf02ae041f9
ChrystianOrren0 <70238350+ChrystianOrren0@users.noreply.github.com>
2022-11-01 23:29:33 -0400

    Add files via upload

---
+++ frontend/Mobile-App/LoginScreen.js
@@ -0,0 +1,110 @@
+import { Text, View, StyleSheet, Button, ImageBackground, TextInput, Image, TouchableOpacity } from 'react-native'
+import { useState } from 'react';
+import React from 'react';
+
+const background = '../Images/OCMgradient.png'
+const logo = '../Images/OCMlogo2.png';
+
+
+const LoginScreen = ( {navigation} ) => {
+	const [UN, setUN] = useState("");
+	const [PW, setPW] = useState("");
+
+	const handleLogin = () => {
+		console.log("login api here");
+	}
+
+	const switchS = () => { //handles screen switching
+		navigation.navigate('SignUpScreen');
+	}
+
+	return(
+		<ImageBackground style={styles.background}source={require(background)}>
+			<View>
+				<Text style = {styles.title}>Our Cooking Mama</Text>
+				<Image source={require(logo)} style = {styles.logo}></Image>
+				<TextInput
+					placeholder= "Username"
+					style = {styles.input}
+					onChangeText = {(val) => setUN(val)}
+					backgroundColor = '#fff'
+				/>
+				<TextInput
+					placeholder= "Passowrd"
+					style = {styles.input}
+					onChangeText = {(val) => setPW(val)}
+					backgroundColor = '#fff'
+				/>
+			</View>
+
+			<View>
+				<Text style={styles.button} onPress={handleLogin}>Login</Text>
+			</View>
+
+			<View style ={{flexDirection: 'row', marginTop: 200}}>
+				<Text style={styles.switchButtonColored}>Login</Text> 
+				<Text> </Text>
+				<Text style = {styles.switchButton} onPress={switchS}>Sign Up</Text>
+			</View>
+
+		</ImageBackground>
+	);
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+	title: {
+		fontSize: 35,
+		alignItems: 'center',
+		marginTop: 100,
+	},
+	background: {
+		flex: 1,
+		alignItems: 'center',
+	},
+	container:{
+		flex: 1,
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+		borderRadius: 20,
+	},
+	logo:{
+		width: 150,
+		height: 150,
+		resizeMode: 'center',
+		marginLeft: 65,
+	},
+	button:{
+		borderWidth: 2,
+		padding: 5,
+		width: 100,
+		textAlign: 'center',
+		backgroundColor: '#E39E6D',
+        borderRadius: 15,
+        overflow: 'hidden',
+	},
+	switchButton:{
+		padding: 7,
+		width: 100,
+		textAlign: 'center',
+		backgroundColor: 'grey',
+        borderRadius: 15,
+        overflow: 'hidden',
+		marginTop: 70,
+	},
+	switchButtonColored:{
+		padding: 7,
+		width: 100,
+		textAlign: 'center',
+		backgroundColor: '#E39E6D',
+        borderRadius: 15,
+        overflow: 'hidden',
+		marginTop: 70,
+	},
+	
+  });
---
+++ frontend/Mobile-App/SignUp.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import { Text, View, StyleSheet, Button, ImageBackground, TextInput, TouchableOpacity } from 'react-native'
+import { useState } from 'react';
+import { NavigationEvents } from 'react-navigation';
+
+const background = '../Images/OCMgradient.png'
+const logo = '../Images/OCMlogo2.png';
+
+const SignUpScreen = ({navigation}) => {
+    const [UN, setUN] = useState("");
+	const [PW, setPW] = useState("");
+    const [PW2, setPW2] = useState("");
+    const [FN, setFN] = useState("");
+	const [LN, setLN] = useState("");
+    const [EM, setEM] = useState("");
+    const comparePass = PW.localeCompare(PW2);
+
+    const handleCreate = () => {
+
+    }
+    
+    const switchL = () => {
+        navigation.navigate('LoginScreen');
+    }
+
+    return(
+        <ImageBackground style={styles.background}source={require(background)}>
+            <View style = {{alignItems: 'center'}}>
+                <Text style = {styles.title}>Sign Up</Text>
+                <Text>Become a chef today!</Text>
+            </View>
+
+            <View>
+            <TextInput
+					placeholder= "Username"
+					style = {styles.input}
+					onChangeText = {(val) => setUN(val)}
+                    backgroundColor = '#fff'
+				/>
+                <TextInput
+					placeholder= "First name"
+					style = {styles.input}
+					onChangeText = {(val) => setFN(val)}
+                    backgroundColor = '#fff'
+				/>
+                <TextInput
+					placeholder= "Last name"
+					style = {styles.input}
+					onChangeText = {(val) => setLN(val)}
+                    backgroundColor = '#fff'
+				/>
+                <TextInput
+					placeholder= "Email"
+					style = {styles.input}
+					onChangeText = {(val) => setEM(val)}
+                    backgroundColor = '#fff'
+				/>
+                <TextInput
+					placeholder= "Password"
+					style = {styles.input}
+					onChangeText = {(val) => setPW(val)}
+                    backgroundColor = '#fff'
+				/>
+                <TextInput
+					placeholder= " Confirm Password"
+					style = {styles.input}
+					onChangeText = {(val) => setPW2(val)}
+                    backgroundColor = '#fff'
+				/>
+            </View>
+
+            <View>
+            <Text style={styles.button} onPress={handleCreate}>Create Account</Text>
+            </View>
+            
+            <View style ={{flexDirection: 'row', marginTop: 135}}>
+				<Text style={styles.switchButton} onPress={switchL}>Login</Text> 
+				<Text> </Text>
+				<Text style = {styles.switchButtonColored}>Sign Up</Text>
+			</View>
+
+        </ImageBackground>
+    );
+}
+
+
+export default SignUpScreen
+
+const styles = StyleSheet.create({
+	title: {
+		fontSize: 35,
+		alignItems: 'center',
+		marginTop: 100,
+	},
+	background: {
+		flex: 1,
+		alignItems: 'center',
+	},
+	container:{
+		flex: 1,
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+        width: 200,
+        borderRadius: 20,
+	},
+	logo:{
+		width: 150,
+		height: 150,
+		resizeMode: 'center',
+		marginLeft: 65,
+	},
+	button:{
+		borderWidth: 2,
+		padding: 5,
+		width: 145,
+		textAlign: 'center',
+		backgroundColor: '#E39E6D',
+        borderRadius: 15,
+        overflow: 'hidden',
+        marginTop: 10,
+	},
+    switchButton:{
+		padding: 7,
+		width: 100,
+		textAlign: 'center',
+		backgroundColor: 'grey',
+        borderRadius: 15,
+        overflow: 'hidden',
+        marginBottom: 70,
+	},
+    switchButtonColored:{
+		padding: 7,
+		width: 100,
+		textAlign: 'center',
+		backgroundColor: '#E39E6D',
+        borderRadius: 15,
+        overflow: 'hidden',
+        marginBottom: 70,
+	},
+	
+  });
