222d0e28bcbbb6a42d60d3b53b52ee39e94db4cc
Fedbca123 <omarshalaby911@knights.ucf.edu>
2022-11-02 21:27:56 -0400

    m

--- frontend/code.js
+++ frontend/code.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
-function doLogin() {}
+function doLogin() {
+	userId = 0;
+	first_name = '';
+	last_name = '';
+
+	let login = document.getElementById('');
+	let password = document.getElementById('');
+
+	document.getElementById('loginResult').innerHTML = '';
+
+	let tmp = { Login: login, Password: password };
+
+	letjsonPayload = JSON.stringify(tmp);
+
+	let url = ulrBase + '/Login.' + extension;
+}
 
 function doRegister() {}
 
