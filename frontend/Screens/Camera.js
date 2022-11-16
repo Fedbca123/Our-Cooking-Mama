64689b54d61ea62bd33182afd1348e6f00834176
ChrystianOrren0 <70238350+ChrystianOrren0@users.noreply.github.com>
2022-11-16 01:51:58 -0500

    Camera funcitonal minus Gallery

--- frontend/Screens/Camera.js
+++ frontend/Screens/Camera.js
@@ -1,13 +1,102 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import BottomTabs from '../components/BottomTabs';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import { useState, useRef, useEffect } from 'react';
+
+
+import Button from '../components/Button.js';
+
+
 
 export default function Settings({navigation}) {
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [image, setImage] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+    const cameraRef = useRef(null);
+
+    useEffect(() => {
+        (async () => {
+            MediaLibrary.requestPermissionsAsync();
+            const cameraStatus = await Camera.requestCameraPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status == 'granted')
+        })();
+    }, [])
+
+    const takePicture = async () => {
+        if(cameraRef){
+            try{
+                const data = await cameraRef.current.takePictureAsync();
+                // console.log(data);
+                setImage(data.uri);
+            }catch(e){
+                console.log(e);
+            }
+        }
+    }
+
+    const saveImage = async  () => {
+        if(image){
+            try{
+                await MediaLibrary.createAssetAsync(image);
+                alert('Saved to camera roll')
+                setImage(null);
+            }catch(e){
+                console.log(e);
+            }
+        }
+
+        navigation.navigate('CreatePost', {img: image});
+
+    }
+
+    if(hasCameraPermission == false){
+        return <Text>No acess to camera</Text> 
+    }
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Our settings page!</Text>
-      <BottomTabs navigation={navigation}/>
-    </View>
+        <View style={styles.container}>
+            {!image ?
+            <Camera 
+                style={styles.camera}
+                type={type}
+                flashMode={flash}
+                ref={cameraRef}
+            >
+
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 30, paddingTop: 40}}>
+                    <Button icon='arrow-long-left' onPress={() => navigation.goBack()}/>
+                    <Button icon={"retweet"} onPress={() => {
+                        setType(type == CameraType.back ? CameraType.front : CameraType.back)
+                    }}/>
+                    <Button
+                        onPress={() => setFlash(flash === Camera.Constants.FlashMode.off
+                        ? Camera.Constants.FlashMode.on
+                        : Camera.Constants.FlashMode.off
+                        )}
+                        icon="flash"
+                        color={flash === Camera.Constants.FlashMode.off ? 'gray' : '#fff'}
+                    />
+                </View>
+
+            </Camera>
+            :
+            <Image source={{uri: image}} style={styles.camera}/>
+            }
+            <View style={styles.camButton}>
+                {image ? 
+                <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+                    <Button title={"Re-take"} icon="retweet" onPress={() => setImage(null)}/>
+                    <Button title={"Save and use"} icon="check" onPress={saveImage}/>
+                </View>
+                :
+                <Button icon="camera" onPress={takePicture} style={styles.takePic}/>
+                }
+            </View>
+        </View>
   );
 }
 
@@ -15,5 +104,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  camButton: {
+    backgroundColor: '#000', 
+    paddingBottom: 30, 
+    paddingTop: 10,
   },
 });
