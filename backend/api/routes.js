1ef36abd49439aed41f346e2d9090dc3e1163e39
Taniya Shaffer <shaffertaniya@gmail.com>
2022-11-16 14:18:02 -0500

    Added frontend CORS headers on route file.

--- backend/api/routes.js
+++ backend/api/routes.js
@@ -6,7 +6,13 @@ const userRegister = require ('../model/userAccount.js');
 const userProfile = require ('../model/userProfile.js');
 const mongoose = require('mongoose');
 
-const DEBUG = true;
+// For frontend CORS
+router.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3002"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // change the 3001 port the port where your webapp is running from!!
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 //Post Method
 router.post('/register', async (req, res) => 
