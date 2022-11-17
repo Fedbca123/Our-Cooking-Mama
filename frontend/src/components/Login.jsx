f13d1450b894f842f3804aae129581af2d342588
Rachel N. Biesiedzinski <85037264+beeinkks@users.noreply.github.com>
2022-11-16 22:20:56 -0500

    test, change back if it doesnt work

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -10,7 +10,7 @@ export const Login = (props) => {
     const [username, setUser] = useState('');
     const [pass, setPass] = useState('');
 
-    const app_name = 'your-cooking-mom-test'
+    const app_name = 'your-cooking-mom'
     function buildPath(route)
     {
         if (process.env.NODE_ENV === 'production') {
@@ -36,7 +36,7 @@ export const Login = (props) => {
                 setMessage("Please Fill in both fields.");
                 return;
             } else { //added else statement so user can't login if they don't fill in both fields.
-                let result = await fetch('http://localhost:3000/api/login', {
+                let result = await fetch(buildPath('api/login'), {
                     method: 'POST',
                     headers: {
                         'Content-Type':'application/json',
