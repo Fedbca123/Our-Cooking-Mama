87b7a9abc9c6de68eac75c8f19ea99e3e81b7236
ChrystianOrren0 <70238350+ChrystianOrren0@users.noreply.github.com>
2022-11-23 13:05:25 -0500

    changed to ioinicons

--- frontend/components/Button.js
+++ frontend/components/Button.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Button({ title, onPress, icon, color }) {
   let camButton;
@@ -12,7 +12,7 @@ export default function Button({ title, onPress, icon, color }) {
 
     return (
       <TouchableOpacity onPress={onPress} style={styles.button}>
-        <Entypo name={icon} size={28} color={color ? color : '#f1f1f1'} />
+        <Ionicons name={icon} size={28} color={color ? color : '#f1f1f1'} />
         <Text style={styles.text}>{title}</Text>
       </TouchableOpacity>
     );
