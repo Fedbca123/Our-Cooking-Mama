8dbadfa47a0c4e47301392598a091f60ee7878fc
Rachel N. Biesiedzinski <85037264+beeinkks@users.noreply.github.com>
2022-11-26 13:50:10 -0500

    functioning display or user data on Profile

--- frontend/src/components/Profile-Components/ProfileSum.js
+++ frontend/src/components/Profile-Components/ProfileSum.js
@@ -3,11 +3,12 @@ import React, { useState } from "react";
 import { buildPath } from "./../bPath";
 
 import Chef from "./../Images/chef.png";
+import { useCookies } from "react-cookie";
 
 export const ProfileSum = (props) => {
 
     //const [modalVisible, setModalVisible] = useState(false);
-    const [nickName, setNickName] = useState('');
+    const [NickName, setNickName] = useState('');
     const [pronouns, setPronouns] = useState('');
     const [accountType, setAccountType] = useState('');
     const [favCuisine, setCuisine] = useState('');
@@ -16,12 +17,15 @@ export const ProfileSum = (props) => {
     const [favFlavor, setFlavor] = useState('');
     const [profilePic, setProfilePic] = useState('');
 
-    let favCuisineArr = [];
-    let favFoodArr = [];
-    let favDrinkArr = [];
-    let favFlavorArr = [];
-    let dietRestArr = [];
-    let foodAllergyArr = [];
+    const [cookies, setCookie] = useCookies(["user"]);
+    //const myCookie = cookies.get('cookie-name');
+
+    React.useEffect(() => {
+        const callAPI = () => { 
+            getData();
+        };
+        callAPI();
+      },[]);
 
     const getData = async () => {
         const response = await fetch(buildPath("api/getOneProfile"), {
@@ -31,7 +35,7 @@ export const ProfileSum = (props) => {
                 'Accept': 'application/json',
             },
             body: JSON.stringify({
-                Query: global._id,
+                Query: cookies.id,
             }),
         }).catch(err => {
             console.log(err);
@@ -39,7 +43,8 @@ export const ProfileSum = (props) => {
         const data = await response.json();
         if (data.error == "User profile not found.") {
             console.log("not slay!")
-        } else {
+        } 
+        else {
             setNickName(data.NickName)
             setPronouns(data.Pronouns)
             setAccountType(data.AccountType)
@@ -48,20 +53,26 @@ export const ProfileSum = (props) => {
             setFood(data.FavFood)
             setFlavor(data.FavoriteFlavor)
             setProfilePic(data.ProfilePhoto)
+
+            //console.log(data.FavCuisine)
+            //console.log(cookies.id)
+            //console.log(data)
         }
     }
+
 
     //const handleModal = () => {
     //    getData();
     //   setModalVisible(true);
     //}
 
+        
     return (
         <div>
             {(profilePic == '')? 
                 <img src={Chef} className="chef" alt="pixelated person with chef coat and chef hat on." />
                 :
-                <img src={{ uri: profilePic }} alt="User's profile photo "/>
+                <img src={profilePic} className="chef"alt="User's profile photo "/>
             }
             <div className="following">
                 Foodies: 1
@@ -70,21 +81,51 @@ export const ProfileSum = (props) => {
             </div>
             
             <div className="bio">
-                Nick-Name: {nickName}
+                {(NickName == '')?
+                    <>No Nick Name</>
+                    :
+                    <>{NickName}</>
+                }
                 <br />
-                Bio:
+                {(pronouns == '')?
+                    <>Our/Cooking/Mama</>
+                    :
+                    <>{pronouns}</>
+                }
                 <br />
+                {(accountType == '')?
+                    <>finish profile set up</>
+                    :
+                    <>{accountType}</>
+                }
+                {accountType}
+                
+            </div>
+
+            <div className="profileFavs">
                 Favorite Cuisine:
+                <br />
+                <>|   {favCuisine}</>
                 <br />
                 Favorite Drink:
                 <br />
+                <>|   {favDrink}</>
+                <br />
+
                 Favorite Food:
                 <br />
+                <>|   {favFood[0]} {favFood[1]} {favFood[2]} {favFood[3]} {favFood[4]} {favFood[5]} {favFood[6]} {favFood[7]} {favFood[8]}</>
+                <br />
+                
                 Favorite Flavor:
+                <br />
+                <>|   {favFlavor[0]} {favFlavor[1]} {favFlavor[3]} {favFlavor[4]} {favFlavor[5]} {favFlavor[6]}</>
+                <br />
+                        
             </div>
 
         </div>
-    )
+    );
 }
 
 export default ProfileSum;
