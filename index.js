d5803d546f90c903576b9c8ae37f50c75c04cf8a
Taniya Shaffer <shaffertaniya@gmail.com>
2022-11-28 14:12:47 -0500

    Removed backend folder

--- index.js
+++ index.js
@@ -44,4 +44,20 @@ app.use((req, res, next) => {
 		"GET, POST, PATCH, DELETE, OPTIONS",
 	);
 	next();
+
+// -------- Heroku deployment -------- 
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  }); 
+}
+// -------- Heroku deployment -------- 
+
+
 });
