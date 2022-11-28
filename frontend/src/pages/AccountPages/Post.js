0ada71605b25c1380685d3a7fd6c1b771290a517
Cristian <christhenkross@gmail.com>
2022-11-27 22:26:06 -0500

    rendering for post
    make it look good!

--- frontend/src/pages/AccountPages/Post.js
+++ frontend/src/pages/AccountPages/Post.js
@@ -8,57 +8,62 @@ function Post({ post }) {
 	const [profileStats, setProfile] = useState([]);
 	const [recipeStats, setRecipe] = useState([]);
 
+	// These will run one time when a post is rendered
 	useEffect(() => {
-        grabProfile();
+		grabProfile();
 		grabRecipe();
-    }, []);
+	}, []);
 
-    const grabProfile = async () => {
-        var obj = { Query: post.ProfileID };
-        var js = JSON.stringify(obj);
-        try {
-            const response = await fetch(buildPath("api/getOneProfile"), {
-                method: "POST",
-                body: js,
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                },
-            });
+	const grabProfile = async () => {
+		var obj = { Query: post.ProfileID };
+		var js = JSON.stringify(obj);
+		try {
+			const response = await fetch(buildPath("api/getOneProfile"), {
+				method: "POST",
+				body: js,
+				headers: {
+					"Content-Type": "application/json",
+					"Accept": "application/json",
+				},
+			});
 
-            var res = JSON.parse(await response.text());
-            setProfile(res)
-        } catch (e) {
-            alert(e.toString());
-            return;
-        }
-    }
+			var res = JSON.parse(await response.text());
+			setProfile(res)
+		} catch (e) {
+			alert(e.toString());
+			return;
+		}
+	}
 
 	const grabRecipe = async () => {
-        var obj = { RecipeID: post.RecipeID };
-        var js = JSON.stringify(obj);
-        try {
-            const response = await fetch(buildPath("api/getRecipe"), {
-                method: "POST",
-                body: js,
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                },
-            });
+		console.log("recipe id Is " + post.RecipeID)
+		var obj = { RecipeID: post.RecipeID };
+		var js = JSON.stringify(obj);
+		try {
+			const response = await fetch(buildPath("api/getRecipe"), {
+				method: "POST",
+				body: js,
+				headers: {
+					"Content-Type": "application/json",
+					"Accept": "application/json",
+				},
+			});
 
-            var res = JSON.parse(await response.text());
-            setRecipe(res)
-        } catch (e) {
-            alert(e.toString());
-            return;
-        }
-    }
+			var res = JSON.parse(await response.text());
+			setRecipe(res)
+		} catch (e) {
+			alert(e.toString());
+			return;
+		}
+	}
 
 	return (
 		<div className="post">
 			<div className="postHeader">
-				<h3>Username: {profileStats.NickName} ({profileStats.pronouns}) </h3>
+				<h3>
+					<img height='50' width='50' src={profileStats.ProfilePhoto}></img>
+					Username: {profileStats.NickName} ({profileStats.Pronouns})
+				</h3>
 			</div>
 
 			<div className="postImage">
@@ -78,7 +83,8 @@ function Post({ post }) {
 			</h4>
 
 			<h4 className="postRecipe">
-				<strong>Recipe Name: </strong>{recipeStats.Recipe}
+				<strong>Recipe Name: </strong>{recipeStats.Recipe} <br></br>
+				<strong>Recipe Ingredients: </strong>{recipeStats.Ingredients}
 			</h4>
 
 			{/* this is the bottom border line lmao why is it like this */}
