0fe775bb576b076ee4acb842fb1f6772aff53bd7
Taniya Shaffer <shaffertaniya@gmail.com>
2022-11-28 12:45:46 -0500

    Added several more endpoints

--- api.test.js
+++ api.test.js
@@ -85,4 +85,109 @@ describe("POST /getOneProfile", () => {
     })
 })
 
-describe("POST ")
+describe("POST /editProfile", () => {
+    test("Returns status code 200 if user exists in db", async () => {
+        const response = await request(appPort).post('/api/editProfile')
+            .send({ 'NickName': 'Johnny D', 
+                    'DietRest': 'None',
+                    'FavCuisine': 'German',
+                    'FavDrink': 'Long Island Tea',
+                    'FavFood':'Schnitzel',
+                    'FoodAllerg': 'None',
+                    'userId': '6384b475299a4a1b3fa20b0d',
+                    'AccountType': 'Personal',
+                    'pronouns': 'he/him',
+                    'file': '',
+                    '_id': '',
+            })
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(200);
+    })
+
+    test("Returns status code 400 if user does not exist in db", async () => {
+        const response = await request(appPort).post('/api/editProfile')
+            .send({ 'NickName': 'Johnny D', 
+                    'DietRest': 'None',
+                    'FavCuisine': 'German',
+                    'FavDrink': 'Long Island Tea',
+                    'FavFood':'Schnitzel',
+                    'FoodAllerg': 'None',
+                    'userId': '6384b475299a4a1b3fa20mom',
+                    'AccountType': 'Personal',
+                    'pronouns': 'he/him',
+                    'file': '',
+                    '_id': '',
+            })
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(200);
+    })
+})
+
+describe("POST /searchProfiles", () => {
+    test("Returns status code 200 if user(s) exist in db", async () => {
+        const response = await request(appPort).post('/api/searchProfiles')
+            .send({Query: 'rleinecker'})
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(200);
+    })
+
+    test("Returns status code 400 if user does not exist in db", async () => {
+        const response = await request(appPort).post('/api/searchProfiles')
+            .send({Query: 'yoMama'})
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(400);
+    })
+}) 
+
+describe("POST /searchPosts", () => {
+    test("Returns status code 200 if post exist in db", async () => {
+        const response = await request(appPort).post('/api/searchPosts')
+            .send({Query: 'spicy'})
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(200);
+    })
+
+    test("Returns status code 400 if post does not exist in db", async () => {
+        const response = await request(appPort).post('/api/searchPosts')
+            .send({Query: 'noTags'})
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(400);
+    })
+
+    test("Returns status code 500 if server is down", async () => {
+        const response = await request(appPort).post('/api/searchPosts')
+            .send({Query: ''})
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(500);
+    })
+}) 
+
+describe("POST /editPost", () => {
+    test("Returns status code 200 if user and post exists", async () => {
+        const response = await request(appPort).post('/api/editPost')
+            .send({ 'RecipeID': '', 
+                    'PostID': '635729f8a909eb70eca16509',
+                    'ProfileID': '6350bacfad0ac0d60ce31e99',
+                    'Category': 'Testing recipeID editPost endpoint v2',
+                    'file':'https://res.cloudinary.com/dp3rrjhdi/image/upload/v1669215474/ttmjw0mil0dlltsfokmm.jpg',
+                    'Caption': 'Changing caption and photo',
+                    'Tags': ['And', 'Some', 'Tags']
+            })
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(200);
+    })
+
+    test("Returns status code 400 if user and post do not exist", async () => {
+        const response = await request(appPort).post('/api/editPost')
+            .send({ 'RecipeID': '', 
+                    'PostID': '',
+                    'ProfileID': '6350bacfad0ac0d60ce31e99',
+                    'Category': 'Testing recipeID editPost endpoint v2',
+                    'file':'https://res.cloudinary.com/dp3rrjhdi/image/upload/v1669215474/ttmjw0mil0dlltsfokmm.jpg',
+                    'Caption': 'Changing caption and photo',
+                    'Tags': ['And', 'Some', 'Tags']
+            })
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(400);
+    })
+})
