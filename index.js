e6ae7f852a765769506719058623b119326f7db3
Taniya Shaffer <shaffertaniya@gmail.com>
2022-12-01 19:16:24 -0500

    Change dirname path and added postinstall

--- index.js
+++ index.js
@@ -73,10 +73,10 @@ __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') 
 {
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
   app.get("/*", (req, res) => {
     res.sendFile(
-      path.join(__dirname, "../client/build", "index.html"),
+      path.join(__dirname, "./frontend/build", "index.html"),
       function (err) {
         if (err) {
           res.status(500).send(err);
