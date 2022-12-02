49a4dea910e3d9371611c98e450e9120fceef01d
Taniya Shaffer <shaffertaniya@gmail.com>
2022-12-01 19:40:25 -0500

    Added back assets to manifest

--- index.js
+++ index.js
@@ -20,26 +20,26 @@ app.use(express.json());
 app.use("/api", routes);
 app.use(cors());
 app.use(bodyParser.json({ extended: false }));
-app.use(
-    // [
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'", "'unsafe-inline'"],
-        connectSrc: ['https://checkout.stripe.com'],
-        frameSrc: ['https://checkout.stripe.com'],
-        childSrc: ['https://checkout.stripe.com'],
-        scriptSrc: ['https://checkout.stripe.com', "'unsafe-inline'"],
-        styleSrc: [
-          'https://fonts.googleapis.com',
-          'https://checkout.stripe.com',
-        ],
-        fontSrc: ['https://fonts.gstatic.com', 'https://fonts.googleapis.com'],
-        imgSrc: ['https://*.stripe.com', 'https://res.cloudinary.com'],
-        baseUri: ["'self'"],
-      },
-    })
-    // ]
-  )
+// app.use(
+//     // [
+//     helmet.contentSecurityPolicy({
+//       directives: {
+//         defaultSrc: ["'self'", "'unsafe-inline'"],
+//         connectSrc: ['https://checkout.stripe.com'],
+//         frameSrc: ['https://checkout.stripe.com'],
+//         childSrc: ['https://checkout.stripe.com'],
+//         scriptSrc: ['https://checkout.stripe.com', "'unsafe-inline'"],
+//         styleSrc: [
+//           'https://fonts.googleapis.com',
+//           'https://checkout.stripe.com',
+//         ],
+//         fontSrc: ['https://fonts.gstatic.com', 'https://fonts.googleapis.com'],
+//         imgSrc: ['https://*.stripe.com', 'https://res.cloudinary.com'],
+//         baseUri: ["'self'"],
+//       },
+//     })
+//     // ]
+//   )
 
 app.listen(port, () => {
 	console.log("Server started at port: " + port);
