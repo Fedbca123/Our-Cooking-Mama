b81d755e7e0dabbe24f8f1d806822ef50a556f36
Taniya Shaffer <shaffertaniya@gmail.com>
2022-12-01 20:12:36 -0500

    Changed apostrophes and serve http

--- frontend/src/components/bPath.js
+++ frontend/src/components/bPath.js
@@ -2,7 +2,7 @@ const app_name = "our-cooking-mom-test";
 
 exports.buildPath = function buildPath(route) {
 	if (process.env.NODE_ENV === "production") {
-		return "https://" + app_name + ".herokuapp.com/" + route;
+		return "http://" + app_name + ".herokuapp.com/" + route;
 	} else {
 		return "http://localhost:3000/" + route;
 	}
