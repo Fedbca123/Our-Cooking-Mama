1ba6b55e8816e7900ede0b9cac56d691a6c1ebb6
Taniya Shaffer <shaffertaniya@gmail.com>
2022-12-05 16:57:37 -0500

    Reverted some changes made last time

--- frontend/src/components/bPath.js
+++ frontend/src/components/bPath.js
@@ -2,8 +2,8 @@ const app_name = "our-cooking-mom-test";
 
 exports.buildPath = function buildPath(route) {
 	if (process.env.NODE_ENV === "production") {
-		return "https://" + app_name + ".herokuapp.com" + route;
+		return "https://" + app_name + ".herokuapp.com/" + route;
 	} else {
-		return "http://localhost:3000/" + route;
+		return "http://localhost:5000/" + route;
 	}
 };
