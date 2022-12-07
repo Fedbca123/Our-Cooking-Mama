7f2be90d20120f425f12804d1501bd99fc7fce7e
Marc-Anthony Cross <maarccross@gmail.com>
2022-12-06 21:50:03 -0500

    Forcing HTTPS to follow CORS policy.

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ const routes = require("./routes");
 var bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
+function requireHTTPS(req, res, next) {
+	// The 'x-forwarded-proto' check is for Heroku
+	if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+	  return res.redirect('https://' + req.get('host') + req.url);
+	}
+	next();
+}
+
 app.use(express.json());
+app.use(requireHTTPS);
 app.use("/api", routes);
 app.use(cors());
 app.use(bodyParser.json({ extended: false }));
